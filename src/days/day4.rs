95e9c25d00b5f3a25983da4a946db11c40e06145
Erik Sandberg <sandberg.erik@gmail.com>
2022-12-04 08:23:55 +0100

    Day 4

---
+++ src/days/day4.rs
@@ -0,0 +1,75 @@
+use std::str::FromStr;
+
+struct Range {
+    from: u32,
+    to: u32,
+}
+
+impl Range {
+    pub fn fully_contains(&self, other: &Range) -> bool {
+        self.from <= other.from && self.to >= other.to
+    }
+
+    pub fn overlaps(&self, other: &Range) -> bool {
+        self.to >= other.from && self.from <= other.to
+    }
+}
+
+impl FromStr for Range {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut fromto = s.split("-");
+        let from: u32 = fromto.next().expect("Unexpected EOF").parse().expect("Failed to parse to u32");
+        let to: u32 = fromto.next().expect("Unexpected EOF").parse().expect("Failed to parse to u32");
+        Ok(Self { from, to })
+    }
+}
+
+fn count_ranges(lines: &[String], accumulator: fn(Range, Range) -> bool) -> u32 {
+    let mut counted = 0;
+
+    for ranges in lines {
+        let mut splitted = ranges.split(",");
+        let range1: Range = splitted.next().unwrap().parse().unwrap();
+        let range2: Range = splitted.next().unwrap().parse().unwrap();
+
+        if accumulator(range1, range2) {
+            counted += 1;
+        }
+    }
+    counted
+}
+
+fn part1(lines: &[String]) -> u32 {
+    count_ranges(lines, |range1, range2| range1.fully_contains(&range2) || range2.fully_contains(&range1))
+}
+
+fn part2(lines: &[String]) -> u32 {
+    count_ranges(lines, |range1, range2| range1.overlaps(&range2))
+}
+
+pub fn run() -> (u32, u32) {
+    let lines = crate::aoc::lines_from_file("day4.txt");
+    let day1_result = part1(&lines);
+    let day2_result = part2(&lines);
+
+    (day1_result, day2_result)
+}
+
+#[test]
+fn it_works() {
+    let lines: Vec<String> = crate::aoc::lines_from_test(
+        r"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8",
+    );
+
+    let part1_result = part1(&lines);
+    let part2_result = part2(&lines);
+    assert_eq!(2, part1_result);
+    assert_eq!(4, part2_result);
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -2,12 +2,15 @@ mod day1;
 mod day2;
 mod day3;
 
+mod day4;
+
 use counted_array::counted_array;
 
 counted_array!(
     pub const ALL_DAYS: [fn() -> (u32, u32); _] = [
         day1::run,
         day2::run,
-        day3::run
+        day3::run,
+        day4::run
     ]
 );
